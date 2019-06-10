f857401e319c36c116b0ba62f1054a6dd7cc1322
Will Peterson <willpeterson76@gmail.com>
2019-06-10 10:30:07 +0200

    changed bounds of jframe component

--- Segment_Images.py
+++ Segment_Images.py
@@ -7,7 +7,6 @@ img_paths = []
 opened_imgs = []
 dropdown = None
 current_file = None
-shortened_paths = {'': 'file not found'}
 
 def process(imp): 
 	try:
@@ -32,6 +31,7 @@ def select_file(self):
 		dropdown.addItem(path);
 	else:
 		print("file not selected/found")
+	
 
 def add_path_to_dropdown(): 
 	dropdown = JComboBox(img_paths);
@@ -45,11 +45,12 @@ def process_stack(self):
 	for i in range(0, len_stack):
 		temp_file = (dropdown.getItemAt(i));
 		img = IJ.openImage(temp_file);
-		
-	return 0;
+		img = process(img);
+		img.show();
+	return "images processed" if len_stack > 0 else "no images found";
 
 
-def process_img(self): 
+def process_current_img(self): 
 	global current_file
 	current_file = dropdown.getSelectedItem();
 	temp_img = IJ.openImage(current_file);
@@ -60,7 +61,8 @@ def create_gui():
 	global dropdown, current_file
 	frame = JFrame('',
             defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE,
-            size = (300, 300));
+            size = (400, 150));
+	frame.setBounds(350,350,400,150);
 
 	container_panel = JPanel(GridBagLayout());	
 	c = GridBagConstraints();
@@ -73,7 +75,7 @@ def create_gui():
 	c.gridwidth = 3;
 	container_panel.add(dropdown, c);
 
-	add_file_button = JButton('<html>add image</html>', actionPerformed=select_file);
+	add_file_button = JButton('<html>Add Image/File</html>', actionPerformed=select_file);
 	c.fill = GridBagConstraints.HORIZONTAL;
 	c.gridx = 3;
 	c.gridy = 0;
@@ -81,7 +83,7 @@ def create_gui():
 	c.gridwidth = 1;
 	container_panel.add(add_file_button, c);
 	
-	process_file_button = JButton('<html>Process file from dropbox: </html>', actionPerformed=process_img);
+	process_file_button = JButton('<html>Process Selected Image</html>', actionPerformed=process_current_img);
 	c.fill = GridBagConstraints.HORIZONTAL;
 	c.gridx = 0;
 	c.gridy = 1;
@@ -89,7 +91,7 @@ def create_gui():
 	c.gridwidth = 2;
 	container_panel.add(process_file_button, c);
 	
-	process_all_button = JButton('<html>process all</html>', actionPerformed=process_stack);
+	process_all_button = JButton('<html>Process Entire Stack</html>', actionPerformed=process_stack);
 	c.fill = GridBagConstraints.HORIZONTAL;
 	c.gridx = 2;
 	c.gridy = 1;
