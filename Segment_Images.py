1540a44df895347e889e65e1fd67ac821952ff3e
Will Peterson <willpeterson76@gmail.com>
2019-08-25 16:44:49 -0400

    updated dropdown to included shortened path names

--- Segment_Images.py
+++ Segment_Images.py
@@ -3,7 +3,7 @@ from ij.io import OpenDialog
 from ij.process import ImageConverter
 from javax.swing import JFrame, JLabel, JButton, JPanel, JComboBox
 from java.awt import GridBagLayout, GridBagConstraints
-img_paths = []
+img_paths = {}
 opened_imgs = []
 dropdown = None
 current_file = None
@@ -25,16 +25,18 @@ def select_file(self):
 	global img_paths, opened_imgs 
 	path = OpenDialog('Select an image file').getPath();
 	if(path): 
-		img_paths.append(path);
+		shortened_path = path[path.rindex('/')+1:];
+		print(shortened_path);
+		img_paths[shortened_path] = path;
 		opened_imgs.append(IJ.openImage(path));
 		opened_imgs[-1].show();
-		dropdown.addItem(path);
+		dropdown.addItem(shortened_path);
 	else:
 		print("file not selected/found")
 	
 
 def add_path_to_dropdown(): 
-	dropdown = JComboBox(img_paths);
+	dropdown = JComboBox(list(img_paths.keys()));
 	grid_panel.add(dropdown, BorderLayout.CENTER);
 	frame.add(grid_panel);
 
@@ -43,7 +45,7 @@ def process_stack(self):
 	global dropdown
 	len_stack = dropdown.getItemCount();
 	for i in range(0, len_stack):
-		temp_file = (dropdown.getItemAt(i));
+		temp_file = img_paths[(dropdown.getItemAt(i))];
 		img = IJ.openImage(temp_file);
 		img = process(img);
 		img.show();
@@ -52,7 +54,7 @@ def process_stack(self):
 
 def process_current_img(self): 
 	global current_file
-	current_file = dropdown.getSelectedItem();
+	current_file = img_paths[dropdown.getSelectedItem()];
 	temp_img = IJ.openImage(current_file);
 	processed_temp = process(temp_img);
 	processed_temp.show();
@@ -67,7 +69,7 @@ def create_gui():
 	container_panel = JPanel(GridBagLayout());	
 	c = GridBagConstraints();
 	
-	dropdown = JComboBox(img_paths);
+	dropdown = JComboBox(list(img_paths.keys()));
 	c.fill = GridBagConstraints.HORIZONTAL;
 	c.gridx = 0;
 	c.gridy = 0;
